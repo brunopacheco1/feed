e498f7159c8ee8adf73b7422751eafb3020596dd
casa <casa@192.168.0.105>
2015-08-21 22:42:00 -0300

    Removendo classe inutilizada.

--- abstractrobot/src/org/ecad/captacao/exceptions/CrawlingExecutionException.java
+++
@@ -1,9 +0,0 @@
-package org.ecad.captacao.exceptions;
-
-public class CrawlingExecutionException extends Exception {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -3741688931227394980L;
-}
